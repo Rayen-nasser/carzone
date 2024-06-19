8d95cd2c8e00380697281e03f270fe9cf60d2eaa
rayenNasser <111684397+Rayen-nasser@users.noreply.github.com>
2024-06-19 17:59:49 +0100

    Update settings.py

--- carzone/carzone/settings.py
+++ carzone/carzone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y+2h!tx5prko&h!brculg@&26ldnafx@-4+tyk3ozboke960_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -97,17 +97,19 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carzonedb',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'host': 'localhost'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'carzonedb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'host': 'localhost'
+#     }
+# }
 
-DATABASES['default'] = dj_database_url.parse("postgres://carzonedb_9st2_user:foTQO5J5CHn2Fq0Gos7lDtHbRo3uK3xo@dpg-cppe2qqj1k6c73ehh1ug-a.oregon-postgres.render.com/carzonedb_9st2")
+DATABASES ={
+  "default": dj_database_url.parse(os.environ.get("postgres://carzonedb_9st2_user:foTQO5J5CHn2Fq0Gos7lDtHbRo3uK3xo@dpg-cppe2qqj1k6c73ehh1ug-a/carzonedb_9st2"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
